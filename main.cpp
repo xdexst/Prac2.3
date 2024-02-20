0659cfbd4d74a7d4a89b6a3f4552be3b14c6bb91
xdexst <rnoy10303@gmail.com>
2024-02-20 17:59:26 +0400

    Чтение из файлов length и width и выведение обновленных значений

--- main.cpp
+++ main.cpp
@@ -19,3 +19,14 @@ int main() {
     cout << "Width: " << road.width << endl;
 	road.length = 444.4;
     road.width = 444;
+	 cout << "\nAfter access: \n";
+	  ifstream fInLength("C:\\цифро-индустриальное общество\\02_02\\Test2_2\\length.txt");
+    fInLength >> road.length;
+    fInLength.close();
+	  ifstream fInWidth("C:\\цифро-индустриальное общество\\02_02\\Test2_2\\width.txt");
+    fInWidth >> road.width;
+    fInWidth.close();
+	 cout << "Length: " << road.length << endl;
+    cout << "Width: " << road.width << endl;
+    return 0;
+}
